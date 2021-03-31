c2b01198c0ae0497ca309f0ee87e4f8fdcd1129b
bijit027 <bijitdeb70@gmail.com>
2021-03-31 09:24:53 +0600

    add carousal and scroll-down in component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 //npm install react-bootstrap bootstrap
 //npm install --save styled-component
+//use netlify.com for deploy this project
+//npm install typewriter-effect
 import React from 'react';
 
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import Mynavbar from './components/my-navbar/my-navbar.component';
+import MyCarousal from './components/my-carousal/my-carousal.component.jsx';
+import TitleMessage from './components/title-message/title-message.component';
+
 function App() {
   return (
     <div>
       <Mynavbar />
-      My Portfolio
+      <MyCarousal />
+      <TitleMessage />
+    
     </div>
   );
 }
